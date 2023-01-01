5c21256c0c581c02d817573ae2b3b679d0a35f65
Tianyi Zhan <hpyzx666666@gmail.com>
2022-12-31 23:13:46 -0500

    setup project

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,12 @@ import styles from '../styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
+// index in each folder will match all route with that folder
+// next.js use _app.js to initialize pages and taking routes
+// _document.js run at the time server build the object
+//Any <head> code, render logic, load fonts and style sheets should be moved to a custom _document.js
+// Any shared layout between all pages should be moved to a custom _app.js.
+// tailwind css start from small size screen, only need to set exception break point for large screen
 export default function Home() {
   return (
     <>
